52bc8b06b4810e7a9fc857f36ae277eb9decd6a8
aslam <cntctme.aslam@gmail.com>
2021-06-17 17:10:37 +0530

    Portfolio done

--- screens/Portfolio.js
+++ screens/Portfolio.js
@@ -1,15 +1,312 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, FlatList, Image } from "react-native";
+import { connect } from "react-redux";
 import MainLayout from "./MainLayout";
+import { useFocusEffect } from "@react-navigation/native";
+import { getHoldings } from "../stores/market/MarketAction";
+import { BalanceInfo, Chart } from "../components";
+import { SIZES, COLORS, FONTS, dummyData, icons } from "../constants";
 
-const Portfolio = () => {
+const Portfolio = ({ getHoldings, myHoldings }) => {
+  const [selectedCoin, setSelectedCoin] = useState(null);
+  let totalWallet = myHoldings.reduce((a, b) => a + (b.total || 0), 0);
+  let valueChange = myHoldings.reduce(
+    (a, b) => a + (b.holding_value_change_7d || 0),
+    0
+  );
+  let percChange = (valueChange / (totalWallet - valueChange)) * 100;
+
+  useFocusEffect(
+    React.useCallback(() => {
+      getHoldings((holdings = dummyData.holdings));
+    }, [])
+  );
+
+  const renderCurrentBalanceSection = () => {
+    return (
+      <View
+        style={{
+          paddingHorizontal: SIZES.padding,
+          borderBottomRightRadius: 25,
+          borderBottomLeftRadius: 25,
+          backgroundColor: COLORS.gray,
+        }}
+      >
+        <Text
+          style={{
+            marginTop: 20,
+            color: COLORS.white,
+            ...FONTS.largeTitle,
+          }}
+        >
+          Portfolio
+        </Text>
+        <BalanceInfo
+          title="Current Balance"
+          displayAmount={totalWallet}
+          changePct={percChange}
+          containerStyle={{
+            marginTop: SIZES.radius,
+            marginBottom: SIZES.padding,
+          }}
+        />
+      </View>
+    );
+  };
+
   return (
     <MainLayout>
-      <View>
-        <Text>Portfolio</Text>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: COLORS.black,
+        }}
+      >
+        {/* Header - Current Balance */}
+        {renderCurrentBalanceSection()}
+
+        {/* Chart */}
+        <Chart
+          containerStyle={{
+            marginTop: SIZES.radius,
+          }}
+          chartPrices={
+            selectedCoin
+              ? selectedCoin?.sparkline_in_7d?.value
+              : myHoldings[0]?.sparkline_in_7d?.value
+          }
+        />
+        {/* Your Assets */}
+
+        <FlatList
+          data={myHoldings}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={{
+            marginTop: SIZES.padding,
+            paddingHorizontal: SIZES.padding,
+          }}
+          ListHeaderComponent={
+            <View>
+              {/* section title */}
+              <Text
+                style={{
+                  ...FONTS.h2,
+                  color: COLORS.white,
+                  paddingVertical: SIZES.radius,
+                }}
+              >
+                Your Assset
+              </Text>
+
+              {/* header Label */}
+              <View
+                style={{
+                  flexDirection: "row",
+                  marginTop: SIZES.radius,
+                }}
+              >
+                <Text
+                  style={{
+                    flex: 1,
+                    color: COLORS.lightGray3,
+                  }}
+                >
+                  Asset
+                </Text>
+                <Text
+                  style={{
+                    flex: 1,
+                    color: COLORS.lightGray3,
+                    textAlign: "right",
+                  }}
+                >
+                  Price
+                </Text>
+                <Text
+                  style={{
+                    flex: 1,
+                    color: COLORS.lightGray3,
+                    textAlign: "right",
+                    paddingLeft: 15,
+                  }}
+                >
+                  Holdings
+                </Text>
+              </View>
+            </View>
+          }
+          renderItem={({ item }) => {
+            let priceColor =
+              item.price_change_percentage_7d_in_currency == 0
+                ? COLORS.lightGray3
+                : item.price_change_percentage_7d_in_currency > 0
+                ? COLORS.lightGreen
+                : COLORS.red;
+
+            return (
+              <TouchableOpacity
+                style={{
+                  flexDirection: "row",
+                  height: 55,
+                }}
+                onPress={() => setSelectedCoin(item)}
+              >
+                {/* asset */}
+
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <Image
+                    source={{ uri: item.image }}
+                    style={{
+                      width: 20,
+                      height: 20,
+                    }}
+                  />
+                  <Text
+                    style={{
+                      color: COLORS.white,
+                      marginLeft: SIZES.radius,
+                      ...FONTS.h4,
+                    }}
+                  >
+                    {item.name}
+                  </Text>
+                </View>
+                {/* price */}
+                <View
+                  style={{
+                    flex: 1,
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "right",
+                      color: COLORS.white,
+                      ...FONTS.h4,
+                      lineHeight: 15,
+                    }}
+                  >
+                    ${item.current_price.toLocaleString()}
+                  </Text>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    {item.price_change_percentage_7d_in_currency != 0 && (
+                      <Image
+                        source={icons.upArrow}
+                        style={{
+                          height: 10,
+                          width: 10,
+                          tintColor: priceColor,
+                          transform:
+                            item.price_change_percentage_7d_in_currency > 0
+                              ? [{ rotate: "45deg" }]
+                              : [{ rotate: "125deg" }],
+                        }}
+                      />
+                    )}
+                    <Text
+                      style={{
+                        marginLeft: 5,
+                        color: priceColor,
+                        ...FONTS.body5,
+                        lineHeight: 15,
+                      }}
+                    >
+                      {item.price_change_percentage_7d_in_currency.toFixed(2)}%
+                    </Text>
+                  </View>
+                </View>
+                {/* holdings */}
+                <View
+                  style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    paddingLeft: 15,
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "right",
+                      color: COLORS.white,
+                      ...FONTS.h4,
+                      lineHeight: 15,
+                    }}
+                  >
+                    ${item.total.toFixed(2)}
+                  </Text>
+                  <Text
+                    style={{
+                      textAlign: "right",
+                      color: COLORS.lightGray3,
+                      ...FONTS.body5,
+                      lineHeight: 15,
+                    }}
+                  >
+                    {item.qty}
+                    {item.symbol.toUpperCase()}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            );
+          }}
+          ListFooterComponent={
+            <View
+              style={{
+                marginBottom: 50,
+              }}
+            />
+          }
+        />
       </View>
     </MainLayout>
   );
 };
 
-export default Portfolio;
+// export default Portfolio;
+
+function mapStateToProps(state) {
+  return {
+    myHoldings: state.marketReducer.myHoldings,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getHoldings: (
+      holdings,
+      currency,
+      coinList,
+      orderBy,
+      sparkline,
+      priceChangePerc,
+      perPage,
+      page
+    ) => {
+      return dispatch(
+        getHoldings(
+          holdings,
+          currency,
+          coinList,
+          orderBy,
+          sparkline,
+          priceChangePerc,
+          perPage,
+          page
+        )
+      );
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Portfolio);
