31389528a446d0a1652ea8dafc7edb64d22494d3
aslam <cntctme.aslam@gmail.com>
2021-06-17 12:57:42 +0530

    chart section done

--- components/Chart.js
+++ components/Chart.js
@@ -25,8 +25,88 @@ const Chart = ({ containerStyle, chartPrices }) => {
 
   let points = monotoneCubicInterpolation({ data, range: 40 });
 
+  const formatUSD = (value) => {
+    "worklet";
+
+    if (value === "") {
+      return "";
+    }
+    return `$${Number(value).toFixed(2)}`;
+  };
+
+  const formatDateTime = (value) => {
+    "worklet";
+
+    if (value === "") {
+      return "";
+    }
+    var selectedDate = new Date(value * 1000);
+    let date = `0${selectedDate.getDate()}`.slice(-2);
+    let month = `0${selectedDate.getMonth() + 1}`.slice(-2);
+    return `${date} / ${month}`;
+  };
+
+  const formatNumber = (value, roundingPoint) => {
+    if (value > 1e9) {
+      return `${(value / 1e9).toFixed(roundingPoint)}B`;
+    } else if (value > 1e6) {
+      return `${(value / 1e6).toFixed(roundingPoint)}M`;
+    } else if (value > 1e3) {
+      return `${(value / 1e3).toFixed(roundingPoint)}K`;
+    }
+    return value.toFixed(roundingPoint);
+  };
+
+  const getYaxisLabelValue = () => {
+    if (chartPrices != undefined) {
+      let minValue = Math.min(...chartPrices);
+      let maxValue = Math.max(...chartPrices);
+
+      let midValue = (minValue + maxValue) / 2;
+
+      let heigherMidValue = (maxValue + midValue) / 2;
+      let lowerMidValue = (minValue + midValue) / 2;
+
+      let roundingPoint = 2;
+      return [
+        formatNumber(maxValue, roundingPoint),
+        formatNumber(heigherMidValue, roundingPoint),
+        formatNumber(lowerMidValue, roundingPoint),
+        formatNumber(minValue, roundingPoint),
+      ];
+    } else {
+      return [];
+    }
+  };
+
   return (
     <View style={{ ...containerStyle }}>
+      {/* Y axis label */}
+      <View
+        style={{
+          position: "absolute",
+          left: SIZES.padding,
+          top: 0,
+          bottom: 0,
+          justifyContent: "space-between",
+        }}
+      >
+        {getYaxisLabelValue().map((item, index) => {
+          return (
+            <Text
+              style={{
+                color: COLORS.lightGray3,
+                ...FONTS.body4,
+                zIndex: 1000,
+              }}
+              key={index}
+            >
+              {item}
+            </Text>
+          );
+        })}
+      </View>
+
       {/* chart */}
       {data.length > 0 && (
         <ChartPathProvider
@@ -41,6 +121,56 @@ const Chart = ({ containerStyle, chartPrices }) => {
             stroke={COLORS.lightGreen}
             strokeWidth={2}
           />
+          <ChartDot>
+            <View
+              style={{
+                position: "absolute",
+                left: -35,
+                width: 80,
+                alignItems: "center",
+                backgroundColor: COLORS.transparentBlack1,
+              }}
+            >
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: 25,
+                  height: 25,
+                  borderRadius: 15,
+                  backgroundColor: COLORS.white,
+                }}
+              >
+                <View
+                  style={{
+                    width: 15,
+                    height: 15,
+                    borderRadius: 10,
+                    backgroundColor: COLORS.lightGreen,
+                  }}
+                />
+              </View>
+              {/* y label */}
+              <ChartYLabel
+                format={formatUSD}
+                style={{
+                  color: COLORS.white,
+                  ...FONTS.body5,
+                }}
+              />
+
+              {/* x label */}
+
+              <ChartXLabel
+                format={formatDateTime}
+                style={{
+                  color: COLORS.lightGray3,
+                  ...FONTS.body5,
+                  marginTop: -35,
+                }}
+              />
+            </View>
+          </ChartDot>
         </ChartPathProvider>
       )}
     </View>
